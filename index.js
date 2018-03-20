f0a5ab1c9b336dc5bff1bcb7d1b1659f51b0db99
Humayun <humayunkabir.cep@gmail.com>
2018-03-20 20:05:46 +0600

    vue

--- index.js
+++ index.js
@@ -11,33 +11,33 @@ var bodyParser = require('body-parser');
 app.use( bodyParser.json() );
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+	res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 var instance = axios.create({
-  baseURL: 'https://api.imgur.com/3/',
-  headers: { 'Authorization': 'Client-ID ' + process.env.IMGUR_CLIENT_ID }
+	baseURL: 'https://api.imgur.com/3/',
+	headers: { 'Authorization': 'Client-ID ' + process.env.IMGUR_CLIENT_ID }
 });
 
 app.get('/search/:query', function(req, res) {
-  const url = 'gallery/search/top/0/?' + querystring.stringify({ q: req.params.query });
-  instance.get(url)
-    .then(function (result) {
-      res.send(result.data.data.filter(item => !item.is_album && !item.nsfw && !item.animated));
-    })
-    .catch(function (error) {
-      console.log(error);
-    })
-  ;
+	const url = 'gallery/search/top/0/?' + querystring.stringify({ q: req.params.query });
+	instance.get(url)
+		.then(function (result) {
+			res.send(result.data.data.filter(item => !item.is_album && !item.nsfw && !item.animated));
+		})
+		.catch(function (error) {
+			console.log(error);
+		})
+	;
 });
 
 app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 if (process.env.NODE_ENV !== 'production') {
-  require('reload')(server, app);
+	require('reload')(server, app);
 }
 
 server.listen(process.env.PORT, function () {
-  console.log('Listening on port '.concat(process.env.PORT))
+	console.log('Listening on port '.concat(process.env.PORT))
 });
